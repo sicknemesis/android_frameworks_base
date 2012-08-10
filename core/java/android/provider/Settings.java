e0d014fd70375594b1648a118d6e5261dd41b5b3
Danesh Mondegarian <daneshm90@gmail.com>
2012-08-10 14:56:11 -0400

    Navigation bar buttons customization
    Navigation bar buttons customization: Padding fix
    Fix padding calculation
    
    Navigation bar buttons customization: Fix left over glow
    Invalidate all buttons, to avoid nearby glow interference

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2389,6 +2389,13 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY = "status_bar_battery";
 
         /**
+         * Navigation bar customization
+         *
+         * @hide
+         */
+        public static final String NAV_BUTTONS = "nav_buttons";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
