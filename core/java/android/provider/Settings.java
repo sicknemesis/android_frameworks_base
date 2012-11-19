cf83374fb6fa660fd29d5aa6762d1f390ad69f66
Stevespear426 <stephen.k.spear@gmail.com>
2012-11-18 22:34:58 -0500

    Navigation bar Quick launch shortcuts

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2466,6 +2466,17 @@ public final class Settings {
        public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+         * Custom navring actions
+         *
+         * @hide
+         */
+        public static final String[] NAVIGATION_RING_TARGETS = new String[] {
+            "navigation_ring_targets_0",
+            "navigation_ring_targets_1",
+            "navigation_ring_targets_2",
+        };
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
